4784b20b3a95446121ee2b7d150850f7c35d8d2c
athifhere <athifhere@gmail.com>
2022-01-19 10:58:20 +0530

    Add project files.

---
+++ git-experience/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace git_experience.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
